db813fafb35780c0017fc5cd4c9a744df8cdb620
Sagar Borude <87903581+MrSagarRB@users.noreply.github.com>
2022-12-19 13:11:35 +0530

    sagar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   let { user } = useContext(ContextProvider);
   return (
     <div className="test">
-      <Link to="/about">Home </Link>{" "}
+      <Link to="/about">Home </Link> <h1>Sagar</h1>
     </div>
   );
 };
